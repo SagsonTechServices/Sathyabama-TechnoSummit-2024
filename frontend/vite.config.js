3a6db08142290c79ba0a54d239659b1723430b62
SagsonTech Consultancy and Services <sagsontechservices@gmail.com>
2024-09-18 01:54:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,21 @@
-{
-  "builds": [
-    { "src": "frontend/vite.config.js", "use": "@vercel/static-build", "config": { "distDir": "frontend/dist" } }
-  ],
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/frontend/$1" }
-  ],
-  "cleanUrls": true
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure this matches with vercel.json
+  },
+  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Replace with your backend URL and port
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrites URL
+      },
+    },
+  },
+});
