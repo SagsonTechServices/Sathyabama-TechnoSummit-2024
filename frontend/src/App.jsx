b0f6a9ef4c69bd233bd17d31ace2621ec5d0e765
SagsonTech Consultancy and Services <sagsontechservices@gmail.com>
2024-09-18 02:22:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/static/Navbar'
-import Footer from './components/static/Footer'
-import LandingPage from './components/Pages/LandingPage'
-import EventPage from './components/Pages/EventPage'
-import PaymentDetailsPage from './components/Pages/PaymentDetailsPage'
-import VenuesPage from './components/Pages/VenuesPage'
-import RegistrationPage from './components/Pages/RegistrationPage'
-import { Routes, Route } from 'react-router-dom'
-import AboutPage from './components/Pages/AboutPage'
-import PatronsPage from './components/Pages/PatronsPage'
-import SchedulePage from './components/Pages/SchedulePage'
-import RegisteredPageForDeptCoords from './components/Pages/RegisteredPageForDeptCoords'
-import StaffLoginPage from './components/Pages/StaffLoginPage'
-import EventCoordinators from './components/Pages/EventCoordinatorsPage'
-import Guidelines from './components/Pages/GuidelinesPage'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/static/Navbar';
+import Footer from './components/static/Footer';
+import LandingPage from './components/Pages/LandingPage';
+import EventPage from './components/Pages/EventPage';
+import PaymentDetailsPage from './components/Pages/PaymentDetailsPage';
+import VenuesPage from './components/Pages/VenuesPage';
+import RegistrationPage from './components/Pages/RegistrationPage';
+import AboutPage from './components/Pages/AboutPage';
+import PatronsPage from './components/Pages/PatronsPage';
+import SchedulePage from './components/Pages/SchedulePage';
+import RegisteredPageForDeptCoords from './components/Pages/RegisteredPageForDeptCoords';
+import StaffLoginPage from './components/Pages/StaffLoginPage';
+import EventCoordinators from './components/Pages/EventCoordinatorsPage';
+import Guidelines from './components/Pages/GuidelinesPage';
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-
-      <Routes>
-        <Route path='/' element={<LandingPage></LandingPage>}></Route>  
-        <Route path='/about' element={<AboutPage></AboutPage>}></Route>
-        <Route path='/guidelines' element={<Guidelines></Guidelines>}></Route>
-        <Route path='/event/:eventId' element={<EventPage></EventPage>}></Route>  
-        <Route path='/venues' element={<VenuesPage></VenuesPage>}></Route>  
-        <Route path='/payment/details' element={<PaymentDetailsPage></PaymentDetailsPage>}></Route>  
-        <Route path='/event/registration/:eventId' element={<RegistrationPage></RegistrationPage>}></Route>  
-        <Route path='/patrons' element={<PatronsPage></PatronsPage>}></Route>
-        <Route path='/technosummit/events/schedules' element={<SchedulePage></SchedulePage>}></Route>
-        <Route path='/dept-coordinator/registered-students' element={<RegisteredPageForDeptCoords></RegisteredPageForDeptCoords>}></Route>
-        <Route path='/staff/login' element={<StaffLoginPage></StaffLoginPage>}></Route>
-				<Route path='/event-coordinator/registered-students' element={<EventCoordinators></EventCoordinators>}></Route>
-      </Routes>  
-
-      <Footer></Footer>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/guidelines' element={<Guidelines />} />
+          <Route path='/event/:eventId' element={<EventPage />} />
+          <Route path='/venues' element={<VenuesPage />} />
+          <Route path='/payment/details' element={<PaymentDetailsPage />} />
+          <Route path='/event/registration/:eventId' element={<RegistrationPage />} />
+          <Route path='/patrons' element={<PatronsPage />} />
+          <Route path='/technosummit/events/schedules' element={<SchedulePage />} />
+          <Route path='/dept-coordinator/registered-students' element={<RegisteredPageForDeptCoords />} />
+          <Route path='/staff/login' element={<StaffLoginPage />} />
+          <Route path='/event-coordinator/registered-students' element={<EventCoordinators />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
