de394f8adaf548471c4271dfecb80630ca53d73a
Syed Abrar Ahmed G <125363480+SagsonTech@users.noreply.github.com>
2024-09-17 22:51:42 +0530

    Corrections and updations

--- frontend/src/components/app/StaffLoginForm.jsx
+++ frontend/src/components/app/StaffLoginForm.jsx
@@ -56,24 +56,6 @@ function StaffLoginForm() {
       });
   };
 
-	return (
-		<div className="md:px-10 px-4 py-4 primary-shadow rounded-lg md:w-2/5 mx-auto">
-			<div className="label">
-				<span className="label-text font-bold">Your role</span>
-			</div>
-			<select
-				className="select select-bordered w-full"
-				id="role"
-				name="role"
-				value={loginData.role}
-				onChange={handleOnChange}
-			>
-				<option disabled value={""}>
-					I am a...
-				</option>
-				<option value={"deptCoord"}>Department Coordinator</option>
-				<option value={"eventCoord"}>Event Coordinator</option>
-			</select>
   return (
     <div className={`md:px-10 px-4 py-4 primary-shadow rounded-lg md:w-2/5 mx-auto`}>
       <div role="alert" className={`alert alert-error ${isError ? "grid" : "hidden"}`}>
@@ -133,32 +115,6 @@ function StaffLoginForm() {
 			<button className="btn btn-primary w-1/2 mx-auto block mt-10" onClick={handleOnSubmit}>Submit</button>
 		</div>
 	);
-      <InputField
-        label={"Mobile No"}
-        name={"mobileNo"}
-        value={loginData.mobileNo}
-        type={"tel"}
-        placeholder={"Enter your mobile number"}
-        onChange={handleOnChange}
-      ></InputField>
-
-      <InputField
-        label={"Password"}
-        name={"password"}
-        value={loginData.password}
-        type={"password"}
-        placeholder={"Enter your password"}
-        onChange={handleOnChange}
-      ></InputField>
-
-      <button
-        className="btn btn-primary w-1/2 mx-auto block mt-10"
-        onClick={handleOnSubmit}
-      >
-        Submit
-      </button>
-    </div>
-  );
 }
 
 export default StaffLoginForm;
--- frontend/src/components/static/Navbar.jsx
+++ frontend/src/components/static/Navbar.jsx
@@ -1,102 +1,210 @@
-import React from 'react';
-import logo from '../../assets/logo.jpeg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "../../assets/logo.jpeg";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <div>
-            <div className="navbar bg-base-100 navbar-fixed z-50 max-w-screen-2xl container md:px-20 px-2">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-64 p-2 shadow">
-                            <li><Link to={'/'}>Home</Link></li>
-                            <li><Link to={'/about'}>About TechnoSummit</Link></li>
-                            <li><Link to={'/guidelines'}>Guidelines</Link></li>
-                            <li><Link to={'/patrons'}>Our Patrons</Link></li>
-                            <li><Link to={'/technosummit/events/schedules'}>Event Schedules</Link></li>
-                            <li><Link>Download brochure</Link></li>
-                            <li className='md:block hidden'>
-                                <details>
-                                    <summary>Events</summary>
-                                    <ul className="p-2 bg-white text-base-100 font-semibold w-52">
-                                        <li><Link to={'/event/1'}>Coding Challenge</Link></li>
-                                        <li><Link to={'/event/2'}>Design Challenge</Link></li>
-                                        <li><Link to={'/event/3'}>Buildathon</Link></li>
-                                        <li><Link to={'/event/4'}>Technical paper presentation</Link></li>
-                                        <li><Link to={'/event/5'}>Technical quiz</Link></li>
-                                        <li><Link to={'/event/6'}>App Arena</Link></li>
-                                        <li><Link to={'/event/7'}>Technical debate</Link></li>
-                                        <li><Link to={'/event/8'}>Project Exhibition</Link></li>
-                                        <li><Link to={'/event/9'}>Next Gen Gamers</Link></li>
-                                        <li><Link to={'/event/10'}>AR VR Challenges</Link></li>
-                                        <li><Link to={'/event/11'}>Hackathon</Link></li>
-                                        <li><Link to={'/event/12'}>Youtube Challenges</Link></li>
-                                        <li><Link to={'/event/13'}>Food carnival</Link></li>
-                                        <li><Link to={'/event/14'}>Go Karting</Link></li>
-                                        <li><Link to={'/event/15'}>Drones and Robotics Design challenge</Link></li>
-                                    </ul>
-                                </details>
-                            </li>
-                            <li><a href="/venues">Venue Information</a></li>
-                        </ul>
-                    </div>
-                    <a><img src={logo} alt="logo" className='w-32 cursor-pointer' /></a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 font-bold">
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/about'}>About TechnoSummit</Link></li>
-                        <li><Link to={'/guidelines'}>Guidelines</Link></li>
-                        <li><Link to={'/patrons'}>Our Patrons</Link></li>
-                        <li><Link to={'/technosummit/events/schedules'}>Event Schedules</Link></li>
-                        <li><Link>Download brochure</Link></li>
-                        <li><a href="/venues">Venue Information</a></li>
-                        <li>
-                            <details>
-                                <summary>Events</summary>
-                                <ul className="p-2 bg-white text-base-100 font-semibold w-52">
-                                    <li><Link to={'/event/1'}>Coding Challenge</Link></li>
-                                    <li><Link to={'/event/2'}>Design Challenge</Link></li>
-                                    <li><Link to={'/event/3'}>Buildathon</Link></li>
-                                    <li><Link to={'/event/4'}>Technical paper presentation</Link></li>
-                                    <li><Link to={'/event/5'}>Technical quiz</Link></li>
-                                    <li><Link to={'/event/6'}>App Arena</Link></li>
-                                    <li><Link to={'/event/7'}>Technical debate</Link></li>
-                                    <li><Link to={'/event/8'}>Project Exhibition</Link></li>
-                                    <li><Link to={'/event/9'}>Next Gen Gamers</Link></li>
-                                    <li><Link to={'/event/10'}>AR VR Challenges</Link></li>
-                                    <li><Link to={'/event/11'}>Hackathon</Link></li>
-                                    <li><Link to={'/event/12'}>Youtube Challenges</Link></li>
-                                    <li><Link to={'/event/13'}>Food carnival</Link></li>
-                                    <li><Link to={'/event/14'}>Go Karting</Link></li>
-                                    <li><Link to={'/event/15'}>Drones and Robotics Design challenge</Link></li>
-                                </ul>
-                            </details>
-                        </li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <Link to={'/staff/login'}><button className="btn btn-primary btn-sm md:block hidden">Login (Only staff)</button></Link>
-                </div>
+  return (
+    <div>
+      <div className="navbar bg-base-100 navbar-fixed z-50 max-w-screen-2xl container md:px-20 px-2">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-64 p-2 shadow"
+            >
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/about"}>About TechnoSummit</Link>
+              </li>
+              <li>
+                <Link to={"/guidelines"}>Guidelines</Link>
+              </li>
+              <li>
+                <Link to={"/patrons"}>Our Patrons</Link>
+              </li>
+              <li>
+                <Link to={"/technosummit/events/schedules"}>
+                  Event Schedules
+                </Link>
+              </li>
+              <li>
+                <Link>Download brochure</Link>
+              </li>
+              <li className="md:block hidden">
+                <details>
+                  <summary>Events</summary>
+                  <ul className="p-2 bg-white text-base-100 font-semibold w-52">
+                    <li>
+                      <Link to={"/event/1"}>Coding Challenge</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/2"}>Design Challenge</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/3"}>Buildathon</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/4"}>Technical paper presentation</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/5"}>Technical quiz</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/6"}>App Arena</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/7"}>Technical debate</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/8"}>Project Exhibition</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/9"}>Next Gen Gamers</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/10"}>AR VR Challenges</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/11"}>Hackathon</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/12"}>Youtube Challenges</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/13"}>Food carnival</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/14"}>Go Karting</Link>
+                    </li>
+                    <li>
+                      <Link to={"/event/15"}>
+                        Drones and Robotics Design challenge
+                      </Link>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <Link to="/venues">Venue Information</Link>
+              </li>
+              <li>
+                <Link to={"/staff/login"}>
+                    <button className="btn btn-primary btn-sm">Login (Only staff)</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <a>
+            <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+          </a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-bold">
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About TechnoSummit</Link>
+            </li>
+            <li>
+              <Link to={"/guidelines"}>Guidelines</Link>
+            </li>
+            <li>
+              <Link to={"/patrons"}>Our Patrons</Link>
+            </li>
+            <li>
+              <Link to={"/technosummit/events/schedules"}>Event Schedules</Link>
+            </li>
+            <li>
+              <Link>Download brochure</Link>
+            </li>
+            <li>
+              <Link to="/venues">Venue Information</Link>
+            </li>
+            <li>
+              <details>
+                <summary>Events</summary>
+                <ul className="p-2 bg-white text-base-100 font-semibold w-52">
+                  <li>
+                    <Link to={"/event/1"}>Coding Challenge</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/2"}>Design Challenge</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/3"}>Buildathon</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/4"}>Technical paper presentation</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/5"}>Technical quiz</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/6"}>App Arena</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/7"}>Technical debate</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/8"}>Project Exhibition</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/9"}>Next Gen Gamers</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/10"}>AR VR Challenges</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/11"}>Hackathon</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/12"}>Youtube Challenges</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/13"}>Food carnival</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/14"}>Go Karting</Link>
+                  </li>
+                  <li>
+                    <Link to={"/event/15"}>
+                      Drones and Robotics Design challenge
+                    </Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <Link to={"/staff/login"}>
+            <button className="btn btn-primary btn-sm md:block hidden">
+              Login (Only staff)
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
