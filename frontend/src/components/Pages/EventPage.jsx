f3a9f0ad2618801c509ce3b244091cb1e01da6d8
Sailesh223 <subhash250904@gmail.com>
2024-09-18 21:34:30 +0530

    event-page changes and maps updated

--- frontend/src/components/Pages/EventPage.jsx
+++ frontend/src/components/Pages/EventPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Heading2 from '../utils/Heading2';
+import React from 'react'
+import eventsData from '../../../data/eventData'
+import { useParams } from 'react-router-dom'
+import Heading3 from '../utils/Heading3'
+import Badge from '../utils/Badge'
 import AppButton from '../utils/AppButton'
-import Badge from '../utils/Badge';
-import eventsData from '../../../data/eventData';
-import { useParams } from 'react-router-dom';
+import Heading2 from '../utils/Heading2'
 
 function Events() {
     const { eventId } = useParams();
@@ -12,58 +13,85 @@ function Events() {
     if (!eventData) return <h1>Loading...</h1>;
 
     return (
-        <div className='flex flex-col px-10 pt-20 pb-10 md:grid md:grid-cols-10 md:gap-4'>
-            <div className='col-span-4 md:sticky md:top-0 self-start'>
-                <div className='flex flex-col md:hidden'>
-                    <Heading2 text={`${eventData.details.name}`} />
+        <>
+            <div className='flex flex-col px-10 py-20 md:hidden'>
+                <div>
+                    <Heading3 text={`${eventData.details.name}`}></Heading3>
                     <span>
-                        <div className="badge mt-2 badge-warning md:text-lg py-3 rounded-md">
+                        <div className="badge mt-2 badge-warning py-3 rounded-md">
                             {`${eventData.details.feeDetails}`}
                         </div>
                     </span>
-                    <p className='md:text-lg mt-2 mb-5 text-justify'>
+                    <p className=' mt-2 mb-5 text-justify'>
                         {eventData.details.desc}
                     </p>
                 </div>
-                <img src={eventData.details.poster} alt="Event Poster" className='rounded-md' />
-                <div className='md:flex mt-5 justify-center hidden'>
-                    <AppButton btnText={"Register"} link={`/event/registration/${eventData.id}`} />
+                <div className='mb-10'>
+                    <img src={eventData.details.poster} alt="Event Poster" className='rounded-md' />
                 </div>
-            </div>
-
-            <div className='col-span-6 overflow-auto'>
-                <div className='md:flex flex-col hidden'>
-                    <Heading2 text={`${eventData.details.name}`} />
-                    <span>
-                        <div className="badge mt-2 badge-warning text-lg py-3">
-                            {`${eventData.details.feeDetails}`}
-                        </div>
-                    </span>
-                    <p className='text-lg mt-2 mb-5 text-justify'>
-                        {eventData.details.desc}
-                    </p>
-                </div>
-
-                <div className='flex flex-col md:grid grid-cols-10 my-8 md:my-0 md:mb-8 gap-5'>
-                    <div className='flex flex-col w-full col-span-3 gap-5 md:justify-between'>
+                <div>
+                    <div className='flex flex-col w-full col-span-3 gap-5'>
                         <Badge text={`Date: ${eventData.details.date}`} className='p-5 md:p-10' />
                         <Badge text={`Time: ${eventData.details.time}`} className='py-10 px-5 md:py-12' />
                         <Badge text={`Venue: ${eventData.details.venue}`} className='py-12 px-5' />
                     </div>
-
-                    <div className='flex col-span-7'>
-                        <img src={eventData.details.venueImg} alt="Venue" />
-                    </div>
                 </div>
-                <div className='w-full'>
+                <div className='flex flex-col gap-5 my-10'>
+                    <img src={eventData.details.venueImg} alt="Venue" />
                     <img src={eventData.details.mapSS} alt="Map" />
                 </div>
-                <div className='flex mt-5 justify-center md:hidden'>
+                <div className='flex justify-center'>
                     <AppButton btnText={"Register"} link={`/event/registration/${eventData.id}`} />
                 </div>
             </div>
-        </div>
-    );
+
+            <div className='hidden md:block pt-20 pb-10 px-10'>
+                <div className='grid grid-cols-10 gap-5'>
+                    <div className='col-span-3'>
+                        <img src={eventData.details.poster} alt="Event Poster" className='rounded-md sticky top-20' />
+                    </div>
+                    <div className='col-span-7'>
+                        <div className='flex justify-between fixed bg-base-100 mt-[-2px] pb-5 w-4/6'>
+                            <div className='flex flex-col'>
+                                <Heading2 text={`${eventData.details.name}`} />
+                                <span>
+                                    <div className="badge mt-2 badge-warning py-3 rounded-md">
+                                        {`${eventData.details.feeDetails}`}
+                                    </div>
+                                </span>
+                            </div>
+                            <div className='flex justify-center items-center'>
+                                <AppButton btnText={"Register"} link={`/event/registration/${eventData.id}`} />
+                            </div>
+                        </div>
+
+                        <div className='px-4'>
+                            <div className='mt-28'>
+                                <p className=' mt-2 mb-5 text-justify text-lg'>
+                                    {eventData.details.desc}
+                                </p>
+                            </div>
+                            <div className='grid grid-cols-10 gap-5 my-5'>
+                                <div className='flex flex-col w-full col-span-3 gap-5'>
+                                    <Badge text={`Date: ${eventData.details.date}`} className='p-5 md:p-10' />
+                                    <Badge text={`Time: ${eventData.details.time}`} className='py-10 px-5 md:py-12' />
+                                    <Badge text={`Venue: ${eventData.details.venue}`} className='py-12 px-5' />
+                                </div>
+                                <div className='col-span-7'>
+                                    <img src={eventData.details.venueImg} alt="Venue" />
+                                </div>
+                            </div>
+                            <div>
+                                <img src={eventData.details.mapSS} alt="Map" className='w-full h-auto'/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
 }
 
-export default Events;
+export default Events
