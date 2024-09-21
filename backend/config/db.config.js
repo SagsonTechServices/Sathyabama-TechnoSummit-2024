4fc6553a11b66222d17a9b04d12c19cd9694e95b
Syed Abrar Ahmed G <125363480+SagsonTech@users.noreply.github.com>
2024-09-21 06:51:06 +0530

    test

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -8,11 +8,11 @@ console.log('DB_USER:', process.env.DB_USER);
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    port: process.env.PORT,
-    password: process.env.DB_PASSWORD,
+    host: '88.222.244.21',
+    user: 'suraj',
+    database: 'technosummit',
+    port: 3306,
+    password: 'Suraj123*',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
