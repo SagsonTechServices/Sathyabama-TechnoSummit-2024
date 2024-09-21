cd1937ce5222877525f6241f3ae6913157787abe
Syed Abrar Ahmed G <125363480+SagsonTech@users.noreply.github.com>
2024-09-21 06:18:41 +0530

    test

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.PORT,
     password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
